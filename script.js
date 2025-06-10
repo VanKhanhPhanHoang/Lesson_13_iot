09343c04dc3e855d7b6ba6241ebf9c8b37f6f630
adalgiso <vivianephan1512@gmail.com>
2025-06-10 07:04:43 +0000

    Commit

--- script.js
+++ script.js
@@ -1,33 +1,15 @@
-const subscriptionKey = import.meta.env.VITE_SUBSCRIPTION_KEY;
+fetch('./gps-data.json')
+    .then(res => res.json())
+    .then(points => {
+        const map = L.map('map').setView([points[0].lat, points[0].lon], 8);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '© OpenStreetMap contributors'
+        }).addTo(map);
 
-let map, features = [];
-
-fetch("/gps-data.json")
-  .then(res => {
-    if (!res.ok) throw new Error("Không thể tải file JSON");
-    return res.json();
-  })
-  .then(data => {
-    features = data.map(coord => new atlas.data.Feature(
-      new atlas.data.Point([coord.lon, coord.lat])
-    ));
-
-    map = new atlas.Map("myMap", {
-      center: [features[0].geometry.coordinates[0], features[0].geometry.coordinates[1]],
-      zoom: 12,
-      authOptions: {
-        authType: "subscriptionKey",
-        subscriptionKey: subscriptionKey,
-      },
+        points.forEach(p => {
+            L.marker([p.lat, p.lon]).addTo(map);
+        });
+    })
+    .catch(err => {
+        console.error('Lỗi tải JSON:', err);
     });
-
-    map.events.add("ready", function () {
-      const source = new atlas.source.DataSource();
-      map.sources.add(source);
-      map.layers.add(new atlas.layer.BubbleLayer(source));
-      source.add(features);
-    });
-  })
-  .catch(err => {
-    console.error("Lỗi khi tải hoặc xử lý GPS data:", err);
-  });
